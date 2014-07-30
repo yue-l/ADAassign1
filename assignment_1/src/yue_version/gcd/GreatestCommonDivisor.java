d7a5d1d10b03089b4b3318d3771daa26f0b92b25
yue <makingitbettergo@gmail.com>
2014-07-30 23:06:03 +1200

    Created classes for q2

---
+++ assignment_1/src/yue_version/gcd/GreatestCommonDivisor.java
@@ -0,0 +1,22 @@
+package yue_version.gcd;
+
+/**
+ * Greatest Common Divisor
+ * 
+ * @author Yue
+ * @version 30-07-14
+ *
+ */
+public class GreatestCommonDivisor {
+
+	public int gcd(int x, int y) {
+		if (y == 0)
+			return x;
+		return gcd(y, x % y);
+	}
+
+	public static void main(String[] args) {
+		GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+		System.out.println("GCD " + 2 + " and " + 4 + " is " + gcd.gcd(2, 4));
+	}
+}
